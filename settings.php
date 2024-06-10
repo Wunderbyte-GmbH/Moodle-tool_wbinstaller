5e062cd31d420ec0645799c043946887bccee2be
Georg Maißer <georg.maisser@wunderbyte.at>
2024-06-10 13:37:13 +0200

    init

---
+++ settings.php
@@ -0,0 +1,45 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin administration pages are defined here.
+ *
+ * @package     tool_wbinstaller
+ * @category    admin
+ * @copyright   2024 Wunderbyte GmbH <info@wunderbyte.at>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+if ($hassiteconfig) {
+    $settings = new admin_settingpage('tool_wbinstaller_settings', new lang_string('pluginname', 'tool_wbinstaller'));
+    $componentname = 'tool_wbinstaller';
+
+    // phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedIf
+    if ($ADMIN->fulltree) {
+        // TODO: Define actual plugin settings page and add it to the tree - {@link https://docs.moodle.org/dev/Admin_settings}.
+
+    }
+
+    $ADMIN->add(
+        'development',
+        new admin_externalpage(
+            $componentname, get_string('componentname', 'tool_pluginskel'),
+            new moodle_url('/admin/tool/wbinstaller/index.php')
+        )
+    );
+}
