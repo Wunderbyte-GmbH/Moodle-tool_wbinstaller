943a9cb3fccddc7ba5502766be1dbe09b8847c8e
jacob.viertel@wunderbyte.at <jacob.viertel@wunderbyte.at>
2024-10-21 08:29:33 +0200

    Version 0.1.2 (2024102100)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_wbinstaller';
-$plugin->release = '0.1.1';
-$plugin->version = 2024081900;
+$plugin->release = '0.1.2';
+$plugin->version = 2024102100;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
