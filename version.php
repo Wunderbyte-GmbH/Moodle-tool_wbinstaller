4351c8838e95e2bf04af70a65f24ad7b89d439e0
Georg Maißer <georg.maisser@wunderbyte.at>
2024-11-15 09:49:42 +0100

    Version (2024111500)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_wbinstaller';
-$plugin->release = '0.1.3';
-$plugin->version = 2024111300;
+$plugin->release = '0.1.4';
+$plugin->version = 2024111500;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
